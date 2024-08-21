e03b95996383025c172cb8634631dbd429f25453
Tumwebaze Brian <lebriandtb@gmail.com>
2024-08-21 21:38:01 +0300

    Add API project with MassTransit

---
+++ ChapChap.Api/Models/MassTransitOptions.cs
@@ -0,0 +1,15 @@
+﻿namespace ChapChap.Api.Models
+{
+    public class MassTransitOptions
+    {
+        public RabbitMQ? RabbitMQ { get; set; }
+    }
+
+    public class RabbitMQ
+    {
+        public string Host { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string TransactionQueue { get; set; } = string.Empty;
+    }
+}
